2084963b4d6100cc8e2ac30820ae267aeaecedcd
Pulak <pulak.bhattacharyya@fadv.com>
2018-06-15 10:34:00 +0530

    Quiz instruction

---
+++ src/Quiz.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz-data.json');
+
+class Quiz extends Component {
+    constructor (props) {
+        super(props);
+
+        this.setState = {quiz_position: 1};
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='QuizQuestion'>{quizData['quiz_questions'][0]['instruction_text']}</div>
+            </div>
+        )
+    }
+}
+
+export default Quiz;
